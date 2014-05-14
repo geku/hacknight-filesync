cae8681022a4482d3ceaa235266d606d078db191
Georg Kunz <kwd@gmx.ch>
2014-05-14 19:31:33 +0200

    initial server to receive commands

---
+++ server.rb
@@ -0,0 +1,20 @@
+require 'socket'
+
+server = TCPServer.new 2000 # Server bind to port 2000
+loop do
+  client = server.accept    # Wait for a client to connect
+  
+  while command =  client.gets do
+    case command[0..2]
+    when 'QIT'
+      client.puts "LOG received quit"
+      client.close
+    when 'CNG'
+      puts 'received change ' + command[3..-1]
+    else
+      # ignore command
+      client.puts "ERR unknown command"
+    end
+
+  end
+end
